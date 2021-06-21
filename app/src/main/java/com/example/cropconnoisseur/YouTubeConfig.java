348dc9cb45770afa0c9ac0a70a1961eba77ca378
Aksh Shah <123akshshah@gmail.com>
2021-06-21 13:45:40 +0530

    Update YouTubeConfig.java

--- app/src/main/java/com/example/cropconnoisseur/YouTubeConfig.java
+++ app/src/main/java/com/example/cropconnoisseur/YouTubeConfig.java
@@ -5,7 +5,7 @@ public class YouTubeConfig {
     public YouTubeConfig() {
     }
 
-    public static final String API_KEY = "AIzaSyCMvFrMWT3KozMbUtmDBiSKUeSZp9JjDuo";
+    public static final String API_KEY = "YOUR API KEY";
 
     public static String getApiKey() {
         return API_KEY;
